bf482f24ba85ce9c4a7c49d4d820a646e74c2bf9
vyomdwivedi <vyom.dwivedi2008@gmail.com>
2021-05-05 13:34:52 +0530

    first commit

---
+++ lib/addNote.dart
@@ -0,0 +1,120 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'auth.dart';
+
+class AddNote extends StatefulWidget {
+  static String id = "addNote";
+  @override
+  _AddNoteState createState() => _AddNoteState();
+}
+
+class _AddNoteState extends State<AddNote> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  static FirebaseAuth auth = FirebaseAuth.instance;
+  final String uid = auth.currentUser.uid;
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference notes = firestore.collection("notes");
+  String noteTitle, noteBody;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add note'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(40.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Add note',
+                style: TextStyle(
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Title'
+                ),
+                onSaved: (String title) {
+                  noteTitle = title;
+                },
+                validator: (String title) {
+                  if (title.isEmpty) {
+                    return "Please enter the title";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Body note'
+                ),
+                maxLines: 5,
+                onSaved: (String body) {
+                  noteBody = body;
+                },
+                validator: (String body) {
+                  if (body.isEmpty) {
+                    return "Please enter note body";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(height: 16.0),
+              Row(
+                children: [
+                  ElevatedButton(
+                      child: Text('Add note'),
+                      onPressed: () {
+                        if (formKey.currentState.validate() == true) {
+                          formKey.currentState.save();
+
+                          notes.add({
+                            'title': noteTitle,
+                            'body': noteBody,
+                            'uid': auth.currentUser.uid
+                          }).then((value) => {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Added note successfully')))
+                          }).catchError((error) => {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(error)))
+                          });
+
+                          formKey.currentState.reset();
+                        }
+                      }
+                  ),
+                  SizedBox(width: 16.0),
+                  ElevatedButton(
+                    child: Text('View notes'),
+                    onPressed: () {
+                    },
+                  ),
+                  SizedBox(width: 16.0),
+                  ElevatedButton(
+                    child: Text('Logout'),
+                    onPressed: () {
+                      auth.signOut();
+                      Navigator.pushNamed(context, Auth.id);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Logged out user.')));
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/auth.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'addNote.dart';
+
+class Auth extends StatefulWidget {
+  static String id = "auth";
+  @override
+  _AuthState createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  String userEmail, userPassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Auth'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(30.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Authentication',
+                style: TextStyle(
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Email'
+                ),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (String email) {
+                  userEmail = email;
+                },
+                validator: (String email) {
+                  if (email == null || email.isEmpty) {
+                    return "Please enter your email";
+                  }
+
+                  else {
+                    return null;
+                  }
+
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+                keyboardType: TextInputType.visiblePassword,
+                onSaved: (String password) {
+                  userPassword = password;
+                },
+                obscureText: true,
+                validator: (String password) {
+                  if (password == null || password.isEmpty) {
+                    return "Please enter your password";
+                  }
+
+                  else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(height: 24.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: Text('Login'),
+                    onPressed: () {
+                      if (formKey.currentState.validate() == true) {
+                        formKey.currentState.save();
+
+                        try {
+                          final user = auth.signInWithEmailAndPassword(email: userEmail, password: userPassword);
+
+                          if (auth.currentUser != null) {
+                            Navigator.pushNamed(context, AddNote.id);
+                          }
+
+                        } catch (error) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(error)));
+                        }
+                      }
+                    },
+                  ),
+                  SizedBox(width: 16.0),
+                  ElevatedButton(
+                    child: Text('Register'),
+                    onPressed: () async {
+                      if (formKey.currentState.validate() == true) {
+                        formKey.currentState.save();
+
+                        try {
+                          final newUser = await auth.createUserWithEmailAndPassword(email: userEmail, password: userPassword);
+
+                          if (newUser != null) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Registered user successfully.')));
+                            Navigator.pushNamed(context, AddNote.id);
+                          }
+
+                        } on FirebaseAuthException catch (error) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(error.message)));
+                        }
+                      }
+                    },
+                  ),
+                  SizedBox(width: 16.0),
+                  ElevatedButton(
+                    child: Text('Logout'),
+                    onPressed: () {
+                      auth.signOut();
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Logged out successfully.')));
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
